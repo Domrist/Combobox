3b12ce5447c16857832b449c679cd0a24f1b0f04
terminator228 <twelw2006@gmail.com>
2023-02-12 18:16:51 +0300

    qqq

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from tkinter import *
 from tkinter import ttk
- 
+
 root = Tk()
 root.title("Combobox example")
 root.geometry("250x200")
- 
+
+
 def selected(event):
     selection = combobox.get()
     print(selection)
 
-languages = ["Russian", "French", "English", "Deutch"]
-combobox = ttk.Combobox(values=languages, state="readonly")
+
+Game = ["Fortnite", "CS", "Geometry_dash", "Diyng_lite"]
+combobox = ttk.Combobox(values=Game, state="readonly")
 combobox.pack(anchor=NW, fill=X, padx=5, pady=5)
 combobox.bind("<<ComboboxSelected>>", selected)
 print("Programm started")
