0b83bb8ba1891cea77656416be06dbcbd476ddd2
unknown <anthonborod1n@yandex.ru>
2023-02-12 17:41:22 +0300

    'teachers'

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from tkinter import *
 from tkinter import ttk
- 
 root = Tk()
 root.title("Combobox example")
 root.geometry("250x200")
- 
+
+
 def selected(event):
     selection = combobox.get()
     print(selection)
 
-languages = ["Russian", "French", "English", "Deutch"]
-combobox = ttk.Combobox(values=languages, state="readonly")
+
+teachers = ["Vera", "Katya", "Olga", "Sveta"]
+combobox = ttk.Combobox(values=teachers, state="readonly")
 combobox.pack(anchor=NW, fill=X, padx=5, pady=5)
 combobox.bind("<<ComboboxSelected>>", selected)
 print("Programm started")
